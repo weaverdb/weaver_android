c483307936fbffff4c4b071565824ccdd5a3b63f
Myron Scott <myron@weaverdb.org>
2024-07-09 07:45:29 -0700

    update license

--- android/src/androidTest/java/org/weaverdb/android/ExampleInstrumentedTest.java
+++ android/src/androidTest/java/org/weaverdb/android/ExampleInstrumentedTest.java
@@ -1,7 +1,13 @@
-/*
- * Copyright (c) 2024 Myron Scott <myron@weaverdb.org> All rights reserved.
+/*-------------------------------------------------------------------------
+ *
+ *
+ * Copyright (c) 2024, Myron Scott  <myron@weaverdb.org>
+ *
+ * All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
+ *
+ *-------------------------------------------------------------------------
  */
 
 
